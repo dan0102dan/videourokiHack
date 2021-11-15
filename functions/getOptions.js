266ef09208ef6b8bf8e752ef5044be54442ea708
dan0102dan <me@dan0102dan.ru>
2021-11-16 00:39:31 +0300

    updates

--- functions/getOptions.js
+++ functions/getOptions.js
@@ -1,28 +1,56 @@
 export default (question) => {
-	const variants = []
+	const options = []
 
-	for (let i = 1; i <= parseInt('1'.repeat(question.answers.length), 2); i++) { // перебираю вопросы в "двоичной" системе счисления
-		const variant = []
+	switch (Number(question.type)) { // придумываю все возможные ситуации в зависимости от типа задания
+		case 3:
+			console.log('Я пока не умею отвечать на вопросы письменно(')
+			break
+		// case 4:
+		// case 5:
+		// 	for (let i = 0; i < question.answers.length; i++) {
+		// 		const option = { variant: [], answer: [] }
 
-		switch (Number(question.type)) { // придумываю все возможные ситуации в зависимости от типа задания
-			case 4:
-			case 5:
-				for (const [index, value] of [...i.toString(JSON.parse(question.annotation).length).padStart(question.answers.length, '0')].entries())
-					variant.push({ 'answer_id': question.answers[index].id, 'answer': Number(value) + 1 })
-				break
-			case 6:
+		// 		for (let index = 0; index < JSON.parse(question.annotation).length; index++) {
+		// 			console.log(index)
+		// 			const answer = question.answers.at(index)
+
+		// 			option.variant.push({ 'answer_id': answer.id, 'answer': Number(i) + 1 })
+		// 			option.answer.push({ 'judgment': answer.text, 'answer': Number(i) + 1 })
+		// 		}
+
+		// 		options.push(option)
+		// 	}
+		// 	break
+		case 6:
+			for (let i = 1; i <= parseInt('1'.repeat(question.answers.length), 2); i++) { // перебираю вопросы через "двоичную" систему счисления
+				const option = { variant: [], answer: [] }
+
+				for (const [index, value] of [...i.toString(2).padStart(question.answers.length, '0')].entries()) {
+					const answer = question.answers.at(index)
+
+					option.variant.push({ 'answer_id': answer.id, 'answer': Number(value) })
+					option.answer.push({ 'judgment': answer.text, 'answer': Number(value) })
+				}
+
+				options.push(option)
+			}
+			break
+		default:
+			for (let i = 1; i <= parseInt('1'.repeat(question.answers.length), 2); i++) { // перебираю вопросы через "двоичную" систему счисления
+				const option = { variant: [], answer: [] }
+
 				for (const [index, value] of [...i.toString(2).padStart(question.answers.length, '0')].entries())
-					variant.push({ 'answer_id': question.answers[index].id, 'answer': Number(value) })
-				break
-			default:
-				for (const [index, value] of [...i.toString(2).padStart(question.answers.length, '0')].entries())
-					if (value === '1')
-						variant.push(question.answers[index].id)
-				break
-		}
+					if (value === '1') {
+						const answer = question.answers.at(index)
 
-		variants.push(variant)
+						option.variant.push(answer.id)
+						option.answer.push(answer.text)
+					}
+
+				options.push(option)
+			}
+			break
 	}
 
-	return variants
+	return options
 }
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createTest, getOptions, getResults } from './functions/index.js'
 import axios from 'axios'
 
-const id = 7135182
+const id = 9010022
 const user = 'Антон Анатоль'
 const grade = '11'
 
@@ -10,36 +10,35 @@ async function main () {
 
 	for (const question of JSON.parse(test.questions))
 		try {
-			if (question.answers.length) {
-				// console.log('Внимание, вопрос:', question)
-				console.log('Внимание, вопрос:', question.description, 'Варианты ответов:', question.answers, 'Тип:', question.type)
+			// console.log('Внимание, вопрос:', question)
+			console.log('Внимание, вопрос:', question.description, 'Тип:', question.type)
 
-				const variants = getOptions(question) // получаем все возможные варианты, как можно ответить
-				for (const variant of variants) {
-					const test = await createTest(id, user, grade)
-					// console.log('Пробую', variant)
-					await axios.post(
-						`https://videouroki.net/tests/api/save/${test.member.fakeId}/`,
-						{
-							'answer': {
-								'id': question.id,
-								'variants': variant.length > 1 ? variant : variant.at(0)
-							},
-							'member': test.member
-						}
-					)
-
-					const result = await getResults(test.member.uuid)
-					if (result > 0) {
-						console.log('Правильный ответ:', variant)
-						break
+			const options = getOptions(question) // получаем все возможные варианты, как можно ответить
+			for (const option of options) {
+				const test = await createTest(id, user, grade)
+				// console.log('Пробую', variant)
+				await axios.post(
+					`https://videouroki.net/tests/api/save/${test.member.fakeId}/`,
+					{
+						'answer': {
+							'id': question.id,
+							'variants': option.variant.length > 1 ? option.variant : option.variant.at(0)
+						},
+						'member': test.member
 					}
+				)
+
+				const result = await getResults(test.member.uuid)
+				if (result > 0) {
+					console.log('Правильный ответ:', option.answer)
+					break
 				}
 			}
 		}
 		catch (e) {
 			console.log(e)
 		}
+	console.log('все, я спать...')
 }
 
 main()
