765e7f9f16f7912a351165197a5c8427584d5b31
dan0102dan <me@dan0102dan.ru>
2022-02-16 00:10:36 +0300

    убрал try catch, так как он бесполезен

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
 import { createTest, getOptions, getResults } from './functions/index.js'
 import axios from 'axios'
 
-const id = 8755787
+const id = 7247075
 const user = 'Антон Анатоль'
 const grade = '11'
 
 async function main () {
 	const test = await createTest(id, user, grade)
 
-	for (const question of JSON.parse(test.questions))
-		try {
-			// console.log('Внимание, вопрос:', question)
-			console.log('Внимание, вопрос:', question.description, 'Тип:', question.type)
+	for (const question of JSON.parse(test.questions)) {
+		// console.log('Внимание, вопрос:', question)
+		console.log('Внимание, вопрос:', question.description, 'Тип:', question.type)
 
-			const options = getOptions(question) // получаем все возможные варианты, как можно ответить
-			for (const option of options) {
-				const test = await createTest(id, user, grade)
-				// console.log('Пробую', variant)
-				await axios.post(
-					`https://videouroki.net/tests/api/save/${test.member.fakeId}/`,
-					{
-						'answer': {
-							'id': question.id,
-							'variants': option.variant.length > 1 ? option.variant : option.variant.at(0)
-						},
-						'member': test.member
-					}
-				)
-
-				const result = await getResults(test.member.uuid)
-				if (result > 0) {
-					console.log('Правильный ответ:', option.answer, '\n')
-					break
+		const options = getOptions(question) // получаем все возможные варианты, как можно ответить
+		for (const option of options) {
+			const test = await createTest(id, user, grade)
+			// console.log('Пробую', variant)
+			await axios.post(
+				`https://videouroki.net/tests/api/save/${test.member.fakeId}/`,
+				{
+					'answer': {
+						'id': question.id,
+						'variants': option.variant.length > 1 ? option.variant : option.variant.at(0)
+					},
+					'member': test.member
 				}
+			)
+
+			const result = await getResults(test.member.uuid)
+			if (result > 0) {
+				console.log('Правильный ответ:', option.answer, '\n')
+				break
 			}
 		}
-		catch (e) {
-			console.log(e)
-		}
+	}
 	console.log('все, я спать...')
 }
 
